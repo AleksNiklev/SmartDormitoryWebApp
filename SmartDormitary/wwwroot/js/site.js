cdefc81651449191f806d464daef409a16ac7c99
eXtraGoZ <stranged96@gmail.com>
2018-12-06 00:17:22 +0200

    Beautiful Notify

--- SmartDormitary/wwwroot/js/site.js
+++ SmartDormitary/wwwroot/js/site.js
@@ -2,9 +2,23 @@
 "use strict";
 
 var connection = new signalR.HubConnectionBuilder().withUrl("/notifyHub").build();
-connection.on("notify", function (name, value, measurementType) {
-    $.notify("Sensor" + name + " has value of " + value + " " + measurementType + ".", "danger");
-})
+connection.on("notify",
+    function(name, value, measurementType) {
+        $.notify.addStyle('alertNotify',
+            {
+                html:
+                    "<div>" +
+                        "<div class='alert alert-info alert-dismissible' role='alert'>" +
+                            "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+                            "<span data-notify-text/>" +
+                        "</div>" +
+                    "</div>"
+            });
+        $.notify("Sensor" + name + " has value of " + value + " " + measurementType + ".",
+            {
+                style: 'alertNotify'
+            });
+    });
 
 connection.start().catch(function (err) {
     return console.error(err.toString());
